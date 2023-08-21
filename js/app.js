d5560e3f0bb56eb6bbf6e0f290f21f2b3837d6d0
Ahmed Gamal <ahmgy.mail@gmail.com>
2023-08-21 20:07:12 +0300

    create function for computer random choice

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
-console.log("Ahmed");
+// This array to let computer choose from
+let arr = ["rock", "paper", "scissors"];
+
+// Function for random choice
+function getComputerChoice() {
+  let choice = arr[Math.floor(Math.random() * arr.length)];
+  return choice;
+}
+
+// Test the result
+console.log(getComputerChoice());
