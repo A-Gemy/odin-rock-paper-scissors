8d80135171bd9d021bc597bbd258b07018cc2526
Ahmed Gamal <ahmgy.mail@gmail.com>
2023-08-22 02:15:10 +0300

    add play round function

--- js/app.js
+++ js/app.js
@@ -7,5 +7,36 @@ function getComputerChoice() {
   return choice;
 }
 
-// Test the result
-console.log(getComputerChoice());
+// Invoke getComputerChoice function and store returned value into a variable
+const computerSelection = getComputerChoice();
+console.log(`Computer select: ${computerSelection}`);
+
+// Ask user for choose and store the value into a variable
+const playerSelection = prompt(
+  "enter your choice (rock/paper/scissors):"
+).toLowerCase();
+console.log(`Player select: ${playerSelection}`);
+
+// This function compare between player and computer choice and print the winner
+function playRound(playerSelection, computerSelection) {
+  // Solution with ternary
+  if (playerSelection == computerSelection) {
+    return console.log("It's a draw.");
+  } else if (playerSelection == "rock") {
+    return computerSelection == "paper"
+      ? console.log("You Lose. Paper beats Rock")
+      : console.log("You Win! Rock beats Scissors");
+  } else if (playerSelection == "paper") {
+    return computerSelection == "rock"
+      ? console.log("You Win! Paper beats Rock.")
+      : console.log("You lose, Scissors beats Paper");
+  } else if (playerSelection == "scissors") {
+    return computerSelection == "rock"
+      ? console.log("You lose, Rock beats Scissors.")
+      : console.log("You Win! Scissors beats Paper.");
+  } else {
+    return console.log("Invalid Input");
+  }
+}
+
+console.log(playRound(playerSelection, computerSelection));
