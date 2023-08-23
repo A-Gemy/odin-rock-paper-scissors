5da934d865bda5ea6b69ea15101a73573e30ae5c
Ahmed Gamal <ahmgy.mail@gmail.com>
2023-08-24 02:17:02 +0300

    Complete the project V1.0 (console)

--- js/app.js
+++ js/app.js
@@ -1,42 +1,104 @@
 // This array to let computer choose from
 let arr = ["rock", "paper", "scissors"];
+let winners = [];
 
-// Function for random choice
-function getComputerChoice() {
-  let choice = arr[Math.floor(Math.random() * arr.length)];
-  return choice;
+// Start of the game
+function game() {
+  for (let i = 1; i <= 5; i++) {
+    playRound(i);
+  }
+  logsWins();
 }
 
-// Invoke getComputerChoice function and store returned value into a variable
-const computerSelection = getComputerChoice();
-console.log(`Computer select: ${computerSelection}`);
+// play one round
+function playRound(round) {
+  const playerSelection = getPlayerSelection();
+  const computerSelection = getComputerChoice();
+  const winner = checkWinner(playerSelection, computerSelection);
+  winners.push(winner);
+  logRound(playerSelection, computerSelection, winner, round);
+  // console.log(winner);
+}
 
-// Ask user for choose and store the value into a variable
-const playerSelection = prompt(
-  "enter your choice (rock/paper/scissors):"
-).toLowerCase();
-console.log(`Player select: ${playerSelection}`);
+// Function for player choice playerSelection
+function getPlayerSelection() {
+  // Take user input
+  let input = prompt("Paper, Rock or Scissors");
+  // Make sure player select an option
+  while (input == null) {
+    input = prompt("Paper, Rock or Scissors");
+  }
+  input = input.toLowerCase();
+  // Validate user input
+  let check = validate(input);
+  while (check == false) {
+    input = prompt("Please choose one of: Paper, Rock or Scissors");
+    while (input == null) {
+      input = prompt("Paper, Rock or Scissors");
+    }
+    input = input.toLowerCase();
+    check = validate(input);
+  }
+  return input;
+}
 
-// This function compare between player and computer choice and print the winner
-function playRound(playerSelection, computerSelection) {
-  // Solution with ternary
-  if (playerSelection == computerSelection) {
-    return console.log("It's a draw.");
-  } else if (playerSelection == "rock") {
-    return computerSelection == "paper"
-      ? console.log("You Lose. Paper beats Rock")
-      : console.log("You Win! Rock beats Scissors");
-  } else if (playerSelection == "paper") {
-    return computerSelection == "rock"
-      ? console.log("You Win! Paper beats Rock.")
-      : console.log("You lose, Scissors beats Paper");
-  } else if (playerSelection == "scissors") {
-    return computerSelection == "rock"
-      ? console.log("You lose, Rock beats Scissors.")
-      : console.log("You Win! Scissors beats Paper.");
+// Function for computer random choice
+function getComputerChoice() {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
+
+// Validation of user input
+function validate(input) {
+  return arr.includes(input);
+}
+
+// Compare two choices and select winner
+function checkWinner(choiceP, choiceC) {
+  if (choiceP === choiceC) {
+    return "Tie";
+  } else if (
+    (choiceP === "rock" && choiceC === "scissors") ||
+    (choiceP === "paper" && choiceC === "rock") ||
+    (choiceP === "scissors" && choiceC === "paper")
+  ) {
+    return "You Win!";
   } else {
-    return console.log("Invalid Input");
+    return "You Lose!";
   }
 }
 
-console.log(playRound(playerSelection, computerSelection));
+// logs wins
+function logsWins() {
+  let playerWins = winners.filter((item) => item == "You Win!").length;
+  let computerWins = winners.filter((item) => item == "You Lose!").length;
+  let ties = winners.filter((item) => item == "Tie").length;
+
+  console.log("Results:");
+  console.log("Player wins:", playerWins);
+  console.log("Computer wins:", computerWins);
+  console.log("Ties:", ties);
+
+  if (playerWins > computerWins) {
+    console.log("********************");
+    console.log("YOU WON THE GAME, CONGRATULATIONS!");
+    console.log("********************");
+  } else if (playerWins < computerWins) {
+    console.log("********************");
+    console.log("YOU LOSE THIS TIME, TRY AGAIN.");
+    console.log("********************");
+  } else {
+    console.log("********************");
+    console.log("NOBODY WON!");
+    console.log("********************");
+  }
+  winners = [];
+}
+
+// log rounds
+function logRound(playerChoice, computerChoice, winner, round) {
+  console.log("Round:", round);
+  console.log("Player choose:", playerChoice);
+  console.log("Computer choose:", computerChoice);
+  console.log(winner);
+  console.log("--------------------");
+}
